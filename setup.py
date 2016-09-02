0163b3e98ddd78f4d65686aaa58c55c6de39dda5
Michael Howitz <mh@gocept.com>
2016-09-02 08:26:40 +0200

    PEP-8

--- setup.py
+++ setup.py
@@ -20,16 +20,16 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-setup(name='z3c.ptcompat',
-      version='2.0.0a2.dev0',
-      description='Zope-compatible page template engine based on Chameleon.',
-      long_description=(
-        '.. contents::\n\n' +
-        read('README.txt')
-        + '\n\n' +
-        read('CHANGES.txt')
-        ),
-      classifiers=[
+setup(
+    name='z3c.ptcompat',
+    version='2.0.0a2.dev0',
+    description='Zope-compatible page template engine based on Chameleon.',
+    long_description='\n\n'.join((
+        '.. contents::',
+        read('README.txt'),
+        read('CHANGES.txt'),
+    )),
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -50,30 +50,30 @@ setup(name='z3c.ptcompat',
         'Framework :: Plone',
         'Framework :: Zope2',
         'Framework :: Zope3',
-        ],
-      keywords='zpt template zope',
-      url='http://pypi.python.org/pypi/z3c.ptcompat',
-      author='Zope Corporation and Contributors',
-      author_email='zope-dev@zope.org',
-      license='ZPL',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['z3c'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'z3c.pt >= 3.0.0a1',
-          'zope.pagetemplate >= 3.6.2',
-          'zope.traversing',
-          ],
-      extras_require=dict(
-          test=[
+    ],
+    keywords='zpt template zope',
+    url='http://pypi.python.org/pypi/z3c.ptcompat',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    license='ZPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'z3c.pt >= 3.0.0a1',
+        'zope.pagetemplate >= 3.6.2',
+        'zope.traversing',
+    ],
+    extras_require=dict(
+        test=[
             'zope.testing',
             'zope.configuration'],
-      ),
-      tests_require=[
+    ),
+    tests_require=[
         'zope.testing',
         'zope.configuration'],
-      test_suite='z3c.ptcompat.tests.test_suite',
-      )
+    test_suite='z3c.ptcompat.tests.test_suite',
+)
