79be1d57e64a0db363e4354c9cb6151453c3d279
Michael Howitz <icemac@gmx.net>
2025-04-09 08:24:57 +0200

    Back to development: 4.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ TESTS_REQUIRE = ["zope.configuration", "zope.testing", "zope.testrunner"]
 
 setup(
     name="z3c.ptcompat",
-    version="4.1",
+    version="4.2.dev0",
     description="Zope-compatible page template engine based on Chameleon.",
     long_description="\n\n".join(
         (".. contents::", read("README.rst"), read("CHANGES.rst"))
